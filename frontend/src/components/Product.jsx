8ab6f8a404928fa1f5356211c923889635608987
vinayak8928 <vinayak8928merawade@gmail.com>
2024-05-23 15:26:19 +0530

    Home Screen changes

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,16 +1,24 @@
-import { Card } from 'react-bootstrap';
+import { Card,Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Rating from './Rating';
+import './Product.css';
 
 const Product = ({ product }) => {
   return (
-    <Card className='my-3 p-3 rounded'>
+    <Col sm={12} md={6} lg={4}>
+    {/* <Card className='my-3 p-3 rounded'> */}
+    <Card className='my-3 p-3 my-card'>
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top' />
+        {/* <Card.Img src={product.image} variant='top' /> */}
+        <Card.Img
+          className="image card-image mx-auto"
+          src={product.image}
+          variant="top"
+        />
       </Link>
 
       <Card.Body>
-        <Link to={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`} className='product-title'>
           <Card.Title as='div' className='product-title'>
             <strong>{product.name}</strong>
           </Card.Title>
@@ -26,6 +34,7 @@ const Product = ({ product }) => {
         <Card.Text as='h3'>Rs. {product.price}</Card.Text>
       </Card.Body>
     </Card>
+    </Col>
   );
 };
 
