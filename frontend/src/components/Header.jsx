ecb2d443940f92ef4c8e0f0623c73b6cba0ddd84
safiyamohsin <01fe20bec332@kletech.ac.in>
2024-05-24 20:37:45 +0530

    chages done

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>Galleria</Navbar.Brand>
+            <Navbar.Brand>Antipode Gallery</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav '>
